f5989339233d965bfb595b1044f33a6465bb7b74
unknown <Administrator@.(none)>
2014-04-15 20:01:47 +0800

    add bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import web
+import web_controller
+hendler = web_controller.Handler()
+
+urls = (
+   '/(.*)', 'Gear',
+)
+class Gear:
+    def GET(self, args = False):
+        return "hello world"
+        return hendler.control(args)
+    def POST(self,args=False):
+        return hendler.control(args)
+
+        
+application = web.application(urls, globals())
+appbot = application.wsgifunc()       
